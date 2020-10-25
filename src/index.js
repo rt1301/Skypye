8d6e165d49cd2ddac2bb333b445a9e501462f761
Rishabh Taparia <rishabh.taparia5@gmail.com>
2020-10-25 16:53:50 +0530

    Added data from static.js file and created store and reducer

--- src/index.js
+++ src/index.js
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+import store from './store'
+function fancyLog() {
+  console.log("%c Rendered with ? ??", "background: purple; color: #FFF");
+  console.log(store.getState());
+}
+const render = ()=>{
+  fancyLog();
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+store.subscribe(render);
+render();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
