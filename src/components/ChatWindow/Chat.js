432bd81c049aae5c804d7eb8588f4a82f0f3e96a
Rishabh Taparia <rishabh.taparia5@gmail.com>
2020-10-30 17:36:35 +0530

    Fixed the issue of message input not getting cleared after submitting the form. Chat window scrolls down automatically after sending a message

--- src/components/ChatWindow/Chat.js
+++ src/components/ChatWindow/Chat.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './Chat.css';
 const Chat = ({ message }) => {
 const { text, is_user_msg } = message;
@@ -8,8 +8,15 @@ return (
 );
 };
 const Chats = ({ messages }) => {
+const chatsRef = useRef();
+const scrollToBottom = ()=>{
+    chatsRef.current.scrollTop = chatsRef.current.scrollHeight;
+}
+useEffect(()=>{
+    scrollToBottom();
+},[messages]);
 return (
-<div className="Chats">
+<div className="Chats" ref={chatsRef}>
 {messages.map((message) => (
 <Chat message={message} key={message.number} />
 ))}
--- src/reducers/typing.js
+++ src/reducers/typing.js
@@ -1,9 +1,12 @@
-import {SET_TYPING_VALUE} from '../actions/constants/action-types';
-export default function typing(state='null',action){
+import {SEND_MESSAGE, SET_TYPING_VALUE} from '../actions/constants/action-types';
+export default function typing(state='',action){
     switch(action.type)
     {
         case SET_TYPING_VALUE:{
             return action.payload;
+        }
+        case SEND_MESSAGE:{
+            return "";
         }
         default: return state;
     }
